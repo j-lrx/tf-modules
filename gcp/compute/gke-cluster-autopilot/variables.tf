017b6de583ece70d6e379956c5f90b3f6f4fdc21
j-lrx <49448040+j-lrx@users.noreply.github.com>
2022-02-02 11:49:09 +0100

    Create variables.tf

---
+++ gcp/compute/gke-cluster-autopilot/variables.tf
@@ -0,0 +1,23 @@
+variable "project" {
+  type = string
+}
+variable "name" {
+  type = string
+}
+
+#region or zone
+variable "location" {
+  type    = string
+}
+
+variable "project" {
+  type = string
+}
+
+variable "network" {
+  type = string
+}
+
+variable "subnetwork" {
+  type = string
+}
