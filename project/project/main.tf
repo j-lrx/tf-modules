bc6b1894a7fcaa0e07f1f85c84d372eae06905d8
j-lrx <49448040+j-lrx@users.noreply.github.com>
2022-02-04 22:06:52 +0100

    Update main.tf

--- project/project/main.tf
+++ project/project/main.tf
@@ -1,6 +1,9 @@
 resource "google_project" "my_project" {
-  name       = var.project
-  project_id = var.project_id
-  org_id     = var.org
-  folder_id  = var.folder_id
+  name                = var.project
+  project_id          = var.project_id
+  org_id              = var.org
+  folder_id           = var.folder_id
+  billing_account     = var.billing_account
+  auto_create_network = var.auto_create_network
+  labels              = var.labels
 }
