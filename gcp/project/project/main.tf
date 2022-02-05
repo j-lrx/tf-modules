5b6d200c30c6bfbfe4e0ca64d77c645860556aea
j-lrx <49448040+j-lrx@users.noreply.github.com>
2022-02-05 22:43:37 +0100

    Update main.tf

--- gcp/project/project/main.tf
+++ gcp/project/project/main.tf
@@ -1,6 +1,6 @@
 resource "google_project" "project" {
   name                = var.project
-  project_id          = var.project
+  project_id          = var.project_id
   org_id              = var.org_id
   folder_id           = var.folder_id
   billing_account     = var.billing_account
