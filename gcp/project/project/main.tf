86e74b119d33c56063178d1113b01f5ab01f915c
j-lrx <49448040+j-lrx@users.noreply.github.com>
2022-02-05 22:26:49 +0100

    Update main.tf

--- gcp/project/project/main.tf
+++ gcp/project/project/main.tf
@@ -1,4 +1,4 @@
-resource "google_project" "my_project" {
+resource "google_project" "project" {
   name                = var.project
   project_id          = var.project_id
   org_id              = var.org
