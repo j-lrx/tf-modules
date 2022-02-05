e00d341d2c14c1d6c2480601b32410bafc435d55
j-lrx <49448040+j-lrx@users.noreply.github.com>
2022-02-05 21:18:11 +0100

    Update variables.tf

--- gcp/project/billing-budget/variables.tf
+++ gcp/project/billing-budget/variables.tf
@@ -1,3 +1,12 @@
 variable "billing_account_id" {
   type = string
 }
+variable "display_name" {
+  type = string
+}
+variable "project" {
+  type = string
+}
+variable "pubsub_topic" {
+  type = string
+}
